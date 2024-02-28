b3bbee661b4eede443dedaaa7ad6b4d5dd6cd91f
kumarsachinkain01 <138963538+kumarsachinkain01@users.noreply.github.com>
2024-02-28 18:25:56 +0530

    this is the first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Retrieve the target div element
+const targetDiv = document.getElementById('targetDiv');
+
+// Change CSS properties
+targetDiv.style.backgroundColor = 'yellow';//Change the background color
+
+targetDiv.style.margin = '20px';//Add a margin of 20 pixels to the element 
+
+targetDiv.style.padding = '10px';//Apply a padding of 10 pixels using the style.padding property.
+
+targetDiv.style.fontSize = '18px';//Change the font size to 18 pixels using the style.fontSize property.
+
+
+targetDiv.style.fontWeight = 'bold';//Set the font weight to bold using the style.fontWeight property.
+
+targetDiv.style.height = '200px';//Change the height of the element to 200 pixels
+
+targetDiv.style.width = '300px';//Modify the width of the element to 300 pixels 
